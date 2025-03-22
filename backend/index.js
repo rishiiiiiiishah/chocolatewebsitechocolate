503111f58a35825b05e76c0a8f1e7d5f03895c25
rishih shah <rishiiiishah@gmail.com>
2025-03-22 14:01:19 +0530

    replaceeee

--- backend/index.js
+++ backend/index.js
@@ -9,10 +9,7 @@ const app = express();
 const port = 4000;
 
 // ✅ Middleware setup
-app.use(cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-}));
+app.use(cors());
 app.use(express.json());
 
 // ✅ Connect to MongoDB
